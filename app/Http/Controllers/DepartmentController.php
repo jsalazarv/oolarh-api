78fe7067c790f94de7ba1048b82d8a5109ba989c
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-03-19 10:47:28 -0600

    Department creation added

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDepartmentRequest;
+use App\Models\Department;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreDepartmentRequest $request
+     * @return JsonResponse
+     */
+    public function store(StoreDepartmentRequest $request): JsonResponse
+    {
+        $department = new Department();
+        $department->name = $request->get('name');
+        $department->save();
+
+        return response()->json($department);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
