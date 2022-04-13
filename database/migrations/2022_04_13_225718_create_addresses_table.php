9f8cea0be9a70469976138ba3ce38eb406a4d3b1
alecudec <103142501+alecudec@users.noreply.github.com>
2022-04-13 18:22:59 -0500

    creation of contact tables, address and branches

---
+++ database/migrations/2022_04_13_225718_create_addresses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('country');
+            $table->string('state');
+            $table->string('municipality');
+            $table->string('suburb');
+            $table->string('street');
+            $table->string('outdoor_number');
+            $table->string('interior_number');
+            $table->integer('postal_code');
+            $table->morphs('addressable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
