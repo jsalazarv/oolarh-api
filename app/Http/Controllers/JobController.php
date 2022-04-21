2a816b63f6d43c7698f86bd8d89398f70a5a3a38
Leonardo Liraniponkempo17 <cleonardodaniel@gmail.com>
2022-04-21 14:35:11 -0500

    Job listing functionality added

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -14,9 +14,10 @@ class JobController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $jobs = Job::paginate($request->get("'pageSize', 10"));
+        return JobResource::collection($jobs);
     }
 
     /**
