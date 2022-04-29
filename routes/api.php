24fe190db9313855f5853932cde12d0cb7c1cfc2
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-04-29 15:22:01 -0500

    Added method to log out

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
 
 Route::group(['prefix' => 'applicants', 'middleware' => ['auth:sanctum']], function(){
