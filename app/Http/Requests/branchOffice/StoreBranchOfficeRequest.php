c796d3fa016830e497aaf2ee72fb8123e29db9ed
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-05-04 16:46:57 -0500

    General list of vacancies added

--- app/Http/Requests/branchOffice/StoreBranchOfficeRequest.php
+++ app/Http/Requests/branchOffice/StoreBranchOfficeRequest.php
@@ -21,7 +21,7 @@ class StoreBranchOfficeRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'name' => ['required', 'string', 'max:255'],
--- app/Http/Resources/VacancyResource.php
+++ app/Http/Resources/VacancyResource.php
@@ -15,6 +15,7 @@ class VacancyResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' =>$this->id,
             'name' => $this->name,
             'description' => $this->description,
             'salary' => $this->salary,
